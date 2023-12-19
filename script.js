f5e9346b9a803c2dae4db1092b35271ae2a5df2c
nelson ejali <ejali267@gmail.com>
2023-12-19 12:03:56 +0100

    git

---
+++ script.js
@@ -0,0 +1,35 @@
+const inputBox = document.getElementById('input-box');
+const containerList = document.getElementById('list-container');
+function addTask() {
+  if (inputBox.value === '') {
+    alert('You must write something');
+  } else {
+    let li = document.createElement('li');
+    li.innerHTML = inputBox.value;
+    containerList.appendChild(li);
+    let span = document.createElement('span');
+    span.innerHTML = '\u00d7';
+    li.appendChild(span);
+  }
+  inputBox.value = '';
+  saveData();
+}
+
+containerList.addEventListener('click', function (e) {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked');
+    saveData();
+  } else if (e.target.tagName === 'SPAN') {
+    e.target.parentElement.remove();
+    saveData();
+  }
+});
+
+function saveData() {
+  localStorage.setItem('data', containerList.innerHTML);
+}
+
+function showTask() {
+  containerList.innerHTML = localStorage.getItem('data');
+}
+showTask();
